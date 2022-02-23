f82204817d89da17c259fec7238af900060c32bd
Mohan Dukaan <mohan.kumar@rankz.io>
2022-02-23 13:17:24 +0530

    UI updated

--- src/Componetts/SideBar.js
+++ src/Componetts/SideBar.js
@@ -1,33 +1,23 @@
+import cx from "classnames";
 import React from "react";
 
 export default function SideBar({ data, active, handleActive }) {
   return (
-    <div
-      className="d-flex flex-column vh-100 flex-shrink-0 p-3 pt-4 text-white bg-primary"
-      style={{ width: 240, boxShadow: "rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset" }}
-    >
+    <div className="d-flex flex-column vh-100 flex-shrink-0 p-3 pt-4 text-white bg-primary sidebar">
       <h4 className="d-flex align-items-center justify-content-center m-0 text-white">
         <span className="fs-5 fw-bolder">Dukaan Analytics</span>
       </h4>
       <hr />
-      <ul className="nav nav-pills flex-column mb-auto">
+      <ul className="nav nav-pills mb-auto">
         {data
           .filter(({ values }) => values && values[0]?.formattedValue)
           ?.map(({ values }, idx) => (
             <li
-              className="nav-item"
+              className="nav-item mt-1 w-100"
               key={idx}
               onClick={() => handleActive(values)}
             >
-              <p
-                className={`nav-link fw-4 text-capitalize ${
-                  JSON.stringify(values) === JSON.stringify(active)
-                    ? "active"
-                    : ""
-                }`}
-              >
-                {values[0]?.formattedValue}
-              </p>
+              <Link {...{ values, active }} />
             </li>
           ))}
       </ul>
@@ -35,3 +25,10 @@ export default function SideBar({ data, active, handleActive }) {
     </div>
   );
 }
+
+const Link = ({ values, active }) => {
+  const classes = cx(`nav-link fw-4 text-capitalize`, {
+    active: JSON.stringify(values) === JSON.stringify(active),
+  });
+  return <p className={classes}>{values[0]?.formattedValue}</p>;
+};
